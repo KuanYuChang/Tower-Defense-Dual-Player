bafb3237721759e3a39a3031e1f26b070b6f7a7f
YiYunLiao <kevin19951001@gmail.com>
2016-06-01 22:58:19 +0800

    update GameManual

--- src/main/client/GameManual.java
+++ src/main/client/GameManual.java
@@ -4,12 +4,18 @@ import processing.core.PApplet;
 
 public class GameManual extends GameLayout {
 	private final String title = "Game Manual";
-	//private Button soliderBtn, returnBtn;
+	private Button soliderBtn, returnBtn;
 	
 	//constructor
 	public GameManual(MainApplet parent) {
 		super(parent);
+		this.soliderBtn = new Button(super.getParent(), "Soldier", 570, 420, 250, 50);
+		this.returnBtn = new Button(super.getParent(), "Return", 850, 420, 250, 50);
 	}
+	
+	//getter
+	public Button getSoldierButton(){ return this.soliderBtn; }
+	public Button getReturnButton(){ return this.returnBtn; }
 	
 	//display component
 	@Override
@@ -23,5 +29,7 @@ public class GameManual extends GameLayout {
 		super.getParent().text("Answering volcab. quiz right could gain more golds.", 50, 240);
 		super.getParent().text("GameOver when one's tower's HP turns to zero", 50, 330);
 		super.getParent().textAlign(PApplet.CENTER, PApplet.CENTER);
+		this.soliderBtn.display();
+		this.returnBtn.display();
 	}
 }
