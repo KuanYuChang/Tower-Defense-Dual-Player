2a14b69b3afe484d39541a1dfabc4fd5089cb7c0
103062211 <amanotaiga3665@gmail.com>
2016-06-04 00:31:29 +0800

    create warroir & archer

---
+++ src/main/client/MyArcher.java
@@ -0,0 +1,58 @@
+package main.client;
+
+import de.looksgood.ani.Ani;
+import processing.core.PApplet;
+
+public class MyArcher extends MySoldier{
+	private float weaponPos, arrowPos;
+	
+	//constructor
+	public MyArcher(GameStage gameStage, MainApplet parent, float x, float y){
+		super(gameStage, parent, x, y);
+		this.weaponPos = -super.getSize()/1.5f;
+		this.arrowPos = -super.getSize()/1.5f;
+		this.setupAttackAni();
+	}
+	
+	//display my archer
+	public void display(){
+		//set fill color
+		super.getParent().fill(0, 0, 255, super.getGameStage().getMyArcherLvl()*2);
+		
+		//display soldier
+		super.display();
+		
+		//display weapon
+		super.getParent().noFill();
+		super.getParent().stroke(0);
+		super.getParent().arc(super.getX()-super.getSize()/1.5f, super.getY(), 
+				super.getSize()/1.5f, super.getSize(), 
+				PApplet.PI/2, PApplet.PI/2*3);
+		super.getParent().line(super.getX()+this.weaponPos, super.getY(), 
+				super.getX()-super.getSize()/1.5f, super.getY()-super.getSize());
+		super.getParent().line(super.getX()+this.weaponPos, super.getY(), 
+				super.getX()-super.getSize()/1.5f, super.getY()+super.getSize());
+		super.getParent().stroke(0, 0, 255);
+		super.getParent().line(super.getX()+this.arrowPos, super.getY(), 
+				super.getX()+this.arrowPos-super.getSize()*2, super.getY());
+		
+		if(super.getAniSequence().isEnded()){
+			this.arrowPos = -super.getSize()/1.5f;
+		}
+	}
+	
+	//setup animation for attack
+	@Override
+	public void setupAttackAni() {
+		super.getAniSequence().beginSequence();
+			super.getAniSequence().beginStep();
+				super.getAniSequence().add(Ani.to(this, 0.5f, "weaponPos", super.getSize()/2));
+				super.getAniSequence().add(Ani.to(this, 0.5f, "arrowPos", super.getSize()/2));
+			super.getAniSequence().endStep();
+			super.getAniSequence().beginStep();
+				super.getAniSequence().add(Ani.to(this, 0.5f, "weaponPos", -super.getSize()/1.5f));
+				super.getAniSequence().add(Ani.to(this, 0.5f, "arrowPos", -super.getSize()*2));
+			super.getAniSequence().endStep();
+		super.getAniSequence().endSequence();
+	}
+}
---
+++ src/main/client/MyWarrior.java
@@ -0,0 +1,48 @@
+package main.client;
+
+import de.looksgood.ani.Ani;
+import processing.core.PApplet;
+
+public class MyWarrior extends MySoldier{
+	private float weaponPos;
+	
+	//constructor
+	public MyWarrior(GameStage gameStage, MainApplet parent, float x, float y){
+		super(gameStage, parent, x, y);
+		this.weaponPos = PApplet.PI/4;
+		this.setupAttackAni();
+	}
+	
+	//display my warrior
+	public void display(){
+		//set fill color
+		super.getParent().fill(0, 0, 255, super.getGameStage().getMyWarriorLvl()*2);
+		
+		//display soldier
+		super.display();
+		
+		//display weapon
+		super.getParent().stroke(0);
+		super.getParent().line(super.getX(), super.getY(), 
+				super.getX()+super.getSize()*PApplet.cos(this.weaponPos)*1.5f, 
+				super.getY()-super.getSize()*PApplet.sin(this.weaponPos)*1.5f);
+		super.getParent().line(super.getX(), super.getY(), 
+				super.getX()+super.getSize()*PApplet.cos(this.weaponPos-PApplet.PI/2)/3, 
+				super.getY()-super.getSize()*PApplet.sin(this.weaponPos-PApplet.PI/2)/3);
+		super.getParent().line(super.getX(), super.getY(), 
+				super.getX()+super.getSize()*PApplet.cos(this.weaponPos+PApplet.PI/2)/3, 
+				super.getY()-super.getSize()*PApplet.sin(this.weaponPos+PApplet.PI/2)/3);
+		super.getParent().line(super.getX(), super.getY(), 
+				super.getX()+super.getSize()*PApplet.cos(this.weaponPos+PApplet.PI)/3, 
+				super.getY()-super.getSize()*PApplet.sin(this.weaponPos+PApplet.PI)/3);
+	}
+	
+	//setup animation for attack
+	@Override
+	public void setupAttackAni() {
+		super.getAniSequence().beginSequence();
+			super.getAniSequence().add(Ani.to(this, 0.5f, "weaponPos", PApplet.PI));
+			super.getAniSequence().add(Ani.to(this, 0.5f, "weaponPos", PApplet.PI/4));
+		super.getAniSequence().endSequence();
+	}
+}
