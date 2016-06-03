c711e630176de59697ecf3475607f962e4a3748f
103062211 <amanotaiga3665@gmail.com>
2016-06-03 23:50:48 +0800

    create buttons

---
+++ src/main/client/WarriorButton.java
@@ -0,0 +1,21 @@
+package main.client;
+
+public class WarriorButton extends SoldierButton {
+	private MyWarrior warrior;	
+	
+	//constructor
+	public WarriorButton(GameStage gameStage, MainApplet parent, float x, float y, float width, float height) {
+		super(gameStage, parent, x, y, width, height);
+		this.warrior = new MyWarrior(super.getGameStage(), parent, x, y - height/10);
+	}
+	
+	//display button
+	public void display(){
+		super.display();
+		super.getParent().fill(0);
+		super.getParent().text("Level " + super.getGameStage().getMyWarriorLvl(), 
+				super.getX(), super.getY() - super.getHeight()/2.5f);
+		this.warrior.display();
+		this.warrior.attack();
+	}
+}
